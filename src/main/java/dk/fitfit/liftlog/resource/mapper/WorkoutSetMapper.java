810f586e2d81d2ef3159a88cb77116c43bc9eaf7
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-11-26 19:55:45 +0100

    Having the WorkoutSetMapper use itself caused stack overflow... obviously!

--- src/main/java/dk/fitfit/liftlog/resource/mapper/WorkoutSetMapper.java
+++ src/main/java/dk/fitfit/liftlog/resource/mapper/WorkoutSetMapper.java
@@ -1,33 +1,14 @@
 package dk.fitfit.liftlog.resource.mapper;
 
 import com.google.common.collect.Lists;
-import dk.fitfit.liftlog.domain.User;
 import dk.fitfit.liftlog.domain.WorkoutSet;
-import dk.fitfit.liftlog.resource.UserResource;
 import dk.fitfit.liftlog.resource.WorkoutSetResource;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {UserMapper.class, WorkoutSetMapper.class})
+@Mapper(componentModel = "spring", uses = {UserMapper.class, ExerciseMapper.class})
 interface WorkoutSetMapper extends ClassMapper<WorkoutSet, WorkoutSetResource> {
-/*
-	@Override
-	@Mappings({
-				@Mapping(target = "user", ignore = true),
-				@Mapping(target = "exercise", ignore = true)
-	})
-	WorkoutSet map(WorkoutSetResource resource);
-
-	@Override
-	@Mappings({
-			@Mapping(target = "user", ignore = true),
-			@Mapping(target = "exercise", ignore = true)
-	})
-	WorkoutSetResource map(WorkoutSet resource);
-*/
 	@Override
 	default List<Class<?>> getSupportedClasses() {
 		return Lists.newArrayList(WorkoutSet.class, WorkoutSetResource.class);
