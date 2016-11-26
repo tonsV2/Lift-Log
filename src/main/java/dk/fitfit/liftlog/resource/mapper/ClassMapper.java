f2aff2c67ad120c8b6f29844f3cc18c7e99993cb
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-11-26 15:07:14 +0100

    Removed more hateoas.

--- src/main/java/dk/fitfit/liftlog/resource/mapper/ClassMapper.java
+++ src/main/java/dk/fitfit/liftlog/resource/mapper/ClassMapper.java
@@ -2,13 +2,11 @@ package dk.fitfit.liftlog.resource.mapper;
 
 import dk.fitfit.liftlog.domain.FirstClassDomainObject;
 import dk.fitfit.liftlog.resource.ResourceObject;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 
 public interface ClassMapper<D extends FirstClassDomainObject, R extends ResourceObject> {
 	R map(D domainObject);
-	@Mapping(target = "id", ignore = true) // Ignore self link on incoming resources
 	D map(R resourceObject);
 	List<Class<?>> getSupportedClasses();
 }
--- src/main/java/dk/fitfit/liftlog/resource/mapper/UserMapper.java
+++ src/main/java/dk/fitfit/liftlog/resource/mapper/UserMapper.java
@@ -5,20 +5,13 @@ import dk.fitfit.liftlog.domain.User;
 import dk.fitfit.liftlog.resource.UserResource;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 interface UserMapper extends ClassMapper<User, UserResource> {
 	@Override
-	UserResource map(User user);
-
-	@Override
-	@Mappings({
-			@Mapping(target = "id", ignore = true),
-			@Mapping(target = "workoutSets", ignore = true)
-	})
+	@Mapping(target = "workoutSets", ignore = true)
 	User map(UserResource resource);
 
 	@Override
