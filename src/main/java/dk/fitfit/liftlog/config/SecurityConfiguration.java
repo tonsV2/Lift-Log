868e97a3e1a5db6798126c42ec2da6567e3f4319
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-12-14 22:44:00 +0100

    Now actually using out TokenAuthenticationProvider.

--- src/main/java/dk/fitfit/liftlog/config/SecurityConfiguration.java
+++ src/main/java/dk/fitfit/liftlog/config/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package dk.fitfit.liftlog.config;
 
 import dk.fitfit.liftlog.security.AuthenticationTokenFilter;
+import dk.fitfit.liftlog.security.TokenAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -14,10 +16,12 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 @EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	private final AuthenticationTokenFilter tokenFilter;
+	private final TokenAuthenticationProvider tokenAuthenticationProvider;
 
 	@Autowired
-	public SecurityConfiguration(AuthenticationTokenFilter tokenFilter) {
+	public SecurityConfiguration(AuthenticationTokenFilter tokenFilter, TokenAuthenticationProvider tokenAuthenticationProvider) {
 		this.tokenFilter = tokenFilter;
+		this.tokenAuthenticationProvider = tokenAuthenticationProvider;
 	}
 
 	@Override
@@ -35,5 +39,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 				.exceptionHandling()
 				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"));
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(tokenAuthenticationProvider);
 	}
 }
