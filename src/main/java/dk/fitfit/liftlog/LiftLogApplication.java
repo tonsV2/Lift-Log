bd5f438f7b3bb34a3317893c8beb909d9ab5c7d9
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-12-15 00:52:22 +0100

    Allow api access from all domains.

--- src/main/java/dk/fitfit/liftlog/LiftLogApplication.java
+++ src/main/java/dk/fitfit/liftlog/LiftLogApplication.java
@@ -2,10 +2,25 @@ package dk.fitfit.liftlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class LiftLogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LiftLogApplication.class, args);
+	}
+
+	// Inspiration: https://spring.io/guides/gs/rest-service-cors/
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
