d74b68399fc14874a9a7605aaeb7a3a0133ccf7f
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-12-18 23:49:56 +0100

    Using mapper rather than .of pattern.

--- src/main/java/dk/fitfit/liftlog/resource/assembler/ExerciseAssembler.java
+++ src/main/java/dk/fitfit/liftlog/resource/assembler/ExerciseAssembler.java
@@ -3,6 +3,7 @@ package dk.fitfit.liftlog.resource.assembler;
 import dk.fitfit.liftlog.domain.Exercise;
 import dk.fitfit.liftlog.resource.ExerciseResource;
 import dk.fitfit.liftlog.resource.ResourceContainer;
+import dk.fitfit.liftlog.resource.mapper.ExerciseMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
@@ -13,15 +14,18 @@ import java.util.List;
 @Component
 public class ExerciseAssembler {
 	private final ExerciseLinksAssembler linksAssembler;
+	private final ExerciseMapper exerciseMapper;
 
 	@Autowired
-	public ExerciseAssembler(ExerciseLinksAssembler linksAssembler) {
+	public ExerciseAssembler(ExerciseLinksAssembler linksAssembler, ExerciseMapper exerciseMapper) {
 		this.linksAssembler = linksAssembler;
+		this.exerciseMapper = exerciseMapper;
 	}
 
 	public ExerciseResource toResource(Exercise exercise) {
 		List<Link> links = linksAssembler.getLinks(exercise);
-		ExerciseResource resource = ExerciseResource.from(exercise);
+		//ExerciseResource resource = ExerciseResource.from(exercise);
+		ExerciseResource resource = exerciseMapper.map(exercise);
 		resource.add(links);
 		return resource;
 	}
