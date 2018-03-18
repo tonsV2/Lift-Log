c740ef688230c1a1feef174dab729f805b2d52e8
Sebastian Jensen <sebastianthegreatful@gmail.com>
2018-03-18 20:44:19 +0100

    Add swagger... http://localhost:8080/swagger-ui.html

---
+++ src/main/java/dk/fitfit/liftlog/config/SwaggerConfiguration.java
@@ -0,0 +1,9 @@
+package dk.fitfit.liftlog.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+}
